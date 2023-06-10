c48dd94179117d93d177b03e5b1d1eb258218c8a
LouisaRSantos <louisasantosl12@gmail.com>
2023-06-10 13:49:27 +0800

    created LoginTab

---
+++ app/src/main/java/com/example/barcode_a/LoginTab.kt
@@ -0,0 +1,11 @@
+package com.example.barcode_a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginTab : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_tab)
+    }
+}
