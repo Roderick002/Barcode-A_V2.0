fac528f60be0ddc480aff5afa5afe95a23b55bd9
Roderick002 <104246601+Roderick002@users.noreply.github.com>
2023-06-15 23:37:46 +0800

    Firebase setting up - User database testing

--- app/src/main/java/com/example/barcode_a/RegisterTab.kt
+++ app/src/main/java/com/example/barcode_a/RegisterTab.kt
@@ -6,12 +6,15 @@ import android.os.Bundle
 import android.widget.Toast
 import com.example.barcode_a.databinding.ActivityRegisterTabBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import java.security.AuthProvider
 
 class RegisterTab : AppCompatActivity() {
 
     private lateinit var binding:ActivityRegisterTabBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var  database : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,31 +24,41 @@ class RegisterTab : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
 
+
         binding.btnSignUp.setOnClickListener(){
+            val firstName = binding.etSignUpFirstname.text.toString()
+            val lastName = binding.etSignUpLastname.text.toString()
             val email = binding.etSignUpEmail.text.toString()
             val password = binding.etSignUpPassword.text.toString()
             val confirmpass = binding.etSignUpConfirmPassword.text.toString()
 
             if (email.isNotEmpty() && password.isNotEmpty() && confirmpass.isNotEmpty()){
                 if (password == confirmpass){
+
+                    database = FirebaseDatabase.getInstance().getReference("Users")
+                    val User = User(firstName, lastName, email)
+
+                    database.child(firstName).setValue(User).addOnSuccessListener {
+                        Toast.makeText(this , "Account Created Successfully!" , Toast.LENGTH_SHORT).show()
+                    }.addOnFailureListener(){
+                        Toast.makeText(this , "Account Creation Failed!" , Toast.LENGTH_SHORT).show()
+                    }
+
                     firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
                         if (it.isSuccessful){
+                            firebaseAuth.signOut()
                             val intent = Intent(this, LoginTab::class.java)
                             startActivity(intent)
-                            Toast.makeText(this , "Account Created Successfully!" , Toast.LENGTH_SHORT).show()
-
                         }else{
                             Toast.makeText(this , it.exception.toString() , Toast.LENGTH_SHORT).show()
                         }
                     }
-
                 }else{
                     Toast.makeText(this , "Password doesn't match" , Toast.LENGTH_SHORT).show()
                 }
             }else{
                 Toast.makeText(this , "Empty Fields Are Not Allowed!" , Toast.LENGTH_SHORT).show()
             }
-
         }
     }
 }
