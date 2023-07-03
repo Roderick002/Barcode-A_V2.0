176f0d3461d7bc7c0fbfba91143f649205628f54
LouisaRSantos <louisasantosl12@gmail.com>
2023-07-03 13:09:57 +0800

    added radio buttons for user type

--- app/src/main/java/com/example/barcode_a/RegisterTab.kt
+++ app/src/main/java/com/example/barcode_a/RegisterTab.kt
@@ -5,6 +5,7 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.RadioGroup
 import android.widget.Toast
 import com.example.barcode_a.databinding.ActivityRegisterTabBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -14,9 +15,10 @@ import java.security.AuthProvider
 
 class RegisterTab : AppCompatActivity() {
 
-    private lateinit var binding:ActivityRegisterTabBinding
+    private lateinit var binding: ActivityRegisterTabBinding
     private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var  database : DatabaseReference
+    private lateinit var database: DatabaseReference
+    private lateinit var userType: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +35,23 @@ class RegisterTab : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
 
+        userType = ""
+        binding.rdgUserType.setOnCheckedChangeListener { _, checkedId ->
+            userType = when (checkedId){
+                R.id.rbPersonal -> "Personal"
+                R.id.rbManufacturer -> "Manufacturer"
+                else -> ""
+            }
+        }
+        binding.btnToast.setOnClickListener {
+            if (userType.isNullOrBlank()) {
+                Toast.makeText(this, "Please select a type of user", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, userType, Toast.LENGTH_SHORT).show()
+            }
+        }
 
-        binding.btnSignUp.setOnClickListener(){
+        binding.btnSignUp.setOnClickListener() {
 
             val firstName = binding.etSignUpFirstname.text.toString()
             val lastName = binding.etSignUpLastname.text.toString()
@@ -43,32 +60,46 @@ class RegisterTab : AppCompatActivity() {
             val password = binding.etSignUpPassword.text.toString()
             val confirmpass = binding.etSignUpConfirmPassword.text.toString()
 
-            if (email.isNotEmpty() && password.isNotEmpty() && confirmpass.isNotEmpty()){
-                if (password == confirmpass){
 
-                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
-                        if (it.isSuccessful){
-                            database = FirebaseDatabase.getInstance().getReference("Users")
-                            val User = User(firstName, lastName, email)
+            if (email.isNotEmpty() && password.isNotEmpty() && confirmpass.isNotEmpty()) {
+                if (password == confirmpass) {
 
-                            database.child(userName).setValue(User).addOnSuccessListener {
-                                Toast.makeText(this , "Account Created Successfully!" , Toast.LENGTH_SHORT).show()
-                            }.addOnFailureListener(){
-                                Toast.makeText(this , "Account Creation Failed!" , Toast.LENGTH_SHORT).show()
+                    firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                database = FirebaseDatabase.getInstance().getReference("Users")
+                                val User = User(firstName, lastName, email)
+
+                                database.child(userName).setValue(User).addOnSuccessListener {
+                                    Toast.makeText(
+                                        this,
+                                        "Account Created Successfully!",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }.addOnFailureListener() {
+                                    Toast.makeText(
+                                        this,
+                                        "Account Creation Failed!",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+
+                                firebaseAuth.signOut()
+                                val intent = Intent(this, LoginTab::class.java)
+                                startActivity(intent)
+                            } else {
+                                Toast.makeText(
+                                    this,
+                                    it.exception.toString(),
+                                    Toast.LENGTH_SHORT)
+                                    .show()
                             }
-
-                            firebaseAuth.signOut()
-                            val intent = Intent(this, LoginTab::class.java)
-                            startActivity(intent)
-                        }else{
-                            Toast.makeText(this , it.exception.toString() , Toast.LENGTH_SHORT).show()
                         }
-                    }
-                }else{
-                    Toast.makeText(this , "Password doesn't match" , Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Password doesn't match", Toast.LENGTH_SHORT).show()
                 }
-            }else{
-                Toast.makeText(this , "Empty Fields Are Not Allowed!" , Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Empty Fields Are Not Allowed!", Toast.LENGTH_SHORT).show()
             }
         }
     }
