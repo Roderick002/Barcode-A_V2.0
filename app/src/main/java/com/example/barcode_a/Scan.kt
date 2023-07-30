2c79f840a7e23317bbf409194582571141ebd4bf
LouisaRSantos <louisasantosl12@gmail.com>
2023-07-30 18:00:03 +0800

    Update alarm's compatibility

--- app/src/main/java/com/example/barcode_a/Scan.kt
+++ app/src/main/java/com/example/barcode_a/Scan.kt
@@ -527,7 +527,7 @@ class Scan : Fragment() {
     private fun setAlarm(delayInMilliseconds: Int) {
         val alarmIntent = Intent(requireContext(), AlarmReceiver::class.java)
         val pendingIntent = PendingIntent.getBroadcast(
-            requireContext(), 0, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT
+            requireContext(), 0, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
 
         val triggerTime = SystemClock.elapsedRealtime() + delayInMilliseconds
