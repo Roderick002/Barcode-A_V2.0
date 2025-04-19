cb6c2610062b9633d5aca83b36c1639e863ff9c7
RamZein06 <ramymacevesallena06@gmail.com>
2025-04-19 19:29:18 +0800

    full immersive mode

--- app/src/main/java/com/example/barcode_a/MainActivity.kt
+++ app/src/main/java/com/example/barcode_a/MainActivity.kt
@@ -1,29 +1,13 @@
 package com.example.barcode_a
 
-import android.app.Activity
-import android.graphics.Color
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.BitmapFactory
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.view.View
 import android.view.WindowManager
-import android.widget.Button
-import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.example.barcode_a.databinding.ActivityLoginTabBinding
 import com.example.barcode_a.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
-import androidx.appcompat.widget.Toolbar
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.databinding.DataBindingUtil
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -39,6 +23,7 @@ class MainActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
             )
         }
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -47,26 +32,34 @@ class MainActivity : AppCompatActivity() {
         replaceFragment(Home())
         binding.bottomNavigationView2.setOnItemSelectedListener {
 
-            when(it.itemId){
-
+            when (it.itemId) {
                 R.id.home -> replaceFragment(Home())
                 R.id.scan -> replaceFragment(Scan())
                 R.id.search -> replaceFragment(Search())
-
-                else -> {
-
-                }
             }
             true
         }
-
     }
-    //Toggle Fragments
-    private fun replaceFragment(fragment : Fragment){
+
+    // Enable immersive full screen mode
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            window.decorView.systemUiVisibility =
+                (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        }
+    }
+
+    // Toggle Fragments
+    private fun replaceFragment(fragment: Fragment) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
-
     }
 }
